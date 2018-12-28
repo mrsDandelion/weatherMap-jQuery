3b8f3ee01986bbb49708abd6c18e5c7bf1fa0ec4
Darya_Drachuk <Darya_Drachuk@epam.com>
2018-12-28 14:29:39 +0300

    weatherMap: change http to https

--- src/controller/getInfoWeather.js
+++ src/controller/getInfoWeather.js
@@ -5,7 +5,7 @@ import createMap from "./createMap.js";
 
 export default async function getInfoUsingName(nameCity) {
     $('.wrapperInputText input').data( "value", "true" );
-    await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${nameCity}&appid=${infoWeather.appId}`)
+    await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${nameCity}&appid=${infoWeather.appId}`)
         .then( responce =>{
             $('#map').empty();
             createMap('map', $('.styleMap select').val(), 12, [responce.data.city.coord.lon, responce.data.city.coord.lat]);
