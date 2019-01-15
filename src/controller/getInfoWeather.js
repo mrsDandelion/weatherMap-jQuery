5ec1e6d8837e82c2ffc45715d9eef7722ec7ff56
Darya_Drachuk <Darya_Drachuk@epam.com>
2019-01-15 12:28:55 +0300

    weatherMap: fix bugs

--- src/controller/getInfoWeather.js
+++ src/controller/getInfoWeather.js
@@ -1,17 +1,35 @@
-import axios from "axios";
+//import axios from "axios";
 import infoWeather from "../constans/weather";
 import createViewWeather from './../view/createViewWeather.js'
 import createMap from "./createMap.js";
 
-export default async function getInfoUsingName(nameCity) {
-    $('.wrapperInputText input').data( "value", "true" );
-    await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${nameCity}&appid=${infoWeather.appId}`)
-        .then( responce =>{
-            $('#map').empty();
-            createMap('map', $('.styleMap select').val(), 12, [responce.data.city.coord.lon, responce.data.city.coord.lat]);
-            createViewWeather($('.wrapperDays').empty(), responce.data.list);
+export default function getInfoUsingName(nameCity) {
+    const textInput = $('.wrapperInputText input');
+    if(textInput.val()){
+        textInput.attr( "data-value", true );
+        $.ajax({
+            method: "GET",
+            url:`https://api.openweathermap.org/data/2.5/forecast?q=${nameCity}&appid=${infoWeather.appId}`,
+            cache: false
         })
-        .catch(() => {
-            $('.wrapperInputText input').data( "value", "false" );
+        .done( responce =>{
+            $('#map').empty();
+            createMap('map', $('.styleMap select').val(), 12, [responce.city.coord.lon, responce.city.coord.lat]);
+            createViewWeather($('.wrapperDays').empty(), responce.list);
+        })
+        .fail(() => {
+            textInput.attr( "data-value", false );
+            $('.wrapperDays').empty();
         });
+
+        //await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${nameCity}&appid=${infoWeather.appId}`)
+        //         .then( responce =>{
+        //             $('#map').empty();
+        //             createMap('map', $('.styleMap select').val(), 12, [responce.data.city.coord.lon, responce.data.city.coord.lat]);
+        //             createViewWeather($('.wrapperDays').empty(), responce.data.list);
+        //         })
+        //         .catch(() => {
+        //             $('.wrapperInputText input').attr( "data-value", false );
+        //         });
+    }
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import "./style.css";
 
 createMap('map', $('.styleMap select').val());
 
-$('.alertInfo').hide();
-
 $('.containerInfo input').on('change', function(){
    getInfoUsingName($(this).val());
 });
 
 $('.styleMap select').on('change', function(){
-    createMap('map', $(this).val());
+    getInfoUsingName($('.containerInfo input').val());
 });
