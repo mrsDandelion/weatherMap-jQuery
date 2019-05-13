52a19e33de106f382979b05910d21b02dd7a4b6b
Darya_Drachuk <Darya_Drachuk@epam.com>
2019-05-13 19:00:57 +0300

    weatherMap-es5: change var

--- src/controller/createMap.js
+++ src/controller/createMap.js
@@ -2,7 +2,8 @@ import Map from './../classes/classMap.js';
 import infoMap from './../constans/map.json';
 
 export default function createMap(container, style, zoom = 1, center){
+    var map;
     mapboxgl.accessToken = infoMap.token;
-    var map = new Map(container, style, zoom, center);
+    map = new Map(container, style, zoom, center);
     map.init();
 }
